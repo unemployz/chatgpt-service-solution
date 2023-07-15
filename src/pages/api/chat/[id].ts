08524a0c3001e4697d68edcdd96330a7494ec06d
unemployz <99677923+unemployz@users.noreply.github.com>
2023-07-15 21:24:14 +0000

    Update data

--- src/pages/api/chat/[id].ts
+++ src/pages/api/chat/[id].ts
@@ -1,4 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { queryLanguageModel, formatOpenAIInput } from './index';
 import { getDocumentFromCollectionById } from '../history/[id]';
-import { MongoCli
+import { MongoClient, ObjectId } from 'mongodb';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
