178c8c1f235e2691fe33c8e880ea3cab7ff3f290
unemployz <99677923+unemployz@users.noreply.github.com>
2023-09-28 22:52:24 +0000

    Expanded documentation

--- src/pages/api/chat/index.ts
+++ src/pages/api/chat/index.ts
@@ -1,2 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { MongoClient, Db } fr
+import { MongoClient, Db } from 'mongodb';
+const {encode, decode} = require('gpt-3-encoder')
+const { Configuration, OpenAIApi } = require("openai");
+
+export const end
