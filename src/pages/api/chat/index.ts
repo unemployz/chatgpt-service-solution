8d17fc23b1a53a9c7729b98b337081c0ca1f80b0
unemployz <99677923+unemployz@users.noreply.github.com>
2023-10-04 07:57:43 +0000

    fixed search

--- src/pages/api/chat/index.ts
+++ src/pages/api/chat/index.ts
@@ -14,4 +14,8 @@ export default async function handler(
     res: NextApiResponse<any>
 ) {
     if (req.method !== 'POST') {
-        return res.status(405).
+        return res.status(405).json({
+            error: "Method not allowed"
+        });
+    }
+    const { message, userId } = JSON.parse(r
