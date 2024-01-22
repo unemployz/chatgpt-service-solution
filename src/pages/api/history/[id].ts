be702a2a2938ac41c2ee208344d256568e1ce262
unemployz <99677923+unemployz@users.noreply.github.com>
2024-01-22 07:05:43 +0000

    USB: io_ti: fix firmware download on big-endian machines

--- src/pages/api/history/[id].ts
+++ src/pages/api/history/[id].ts
@@ -1,2 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { MongoClient, ObjectId } from 'mongodb';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    const { id, userId }
