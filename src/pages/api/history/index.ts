81079002b82be3d351428d33aa2c4a55e40cd44a
unemployz <99677923+unemployz@users.noreply.github.com>
2024-03-15 17:00:50 +0000

    Reviewed by Oliver.

--- src/pages/api/history/index.ts
+++ src/pages/api/history/index.ts
@@ -11,4 +11,7 @@ export default async function handler(
 }
 
 async function fetchRecentDocuments(collectionName: string, userId: string) {
-  const 
+  const client = new MongoClient(process.env.MONGO_URL || '');
+  try {
+    await client.connect();
+    const db = client.db(process.e
